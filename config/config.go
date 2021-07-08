933fb1e5cd29ddcb7e1a08169219be9d78cbe9fe
David Subiros <subiros.david@gmail.com>
2021-07-08 17:54:16 +0100

    changed bindAddr so that this can be run by dp-compose

--- config/config.go
+++ config/config.go
@@ -36,7 +36,7 @@ func Get() (*Config, error) {
 	}
 
 	cfg = &Config{
-		BindAddr:                     "localhost:26200",
+		BindAddr:                     ":26200",
 		GracefulShutdownTimeout:      5 * time.Second,
 		HealthCheckInterval:          30 * time.Second,
 		HealthCheckCriticalTimeout:   90 * time.Second,
