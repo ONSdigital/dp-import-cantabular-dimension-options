440138458cdb0975130cb19f7643c1b9227fe171
David Subiros <subiros.david@gmail.com>
2021-07-09 13:49:57 +0100

    use amidos/dcind CI image

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -2,5 +2,7 @@
 
 pushd dp-import-cantabular-dimension-options/features/compose
   echo "hello component tests"
+  docker -v
+  docker-compose version
   docker-compose up --abort-on-container-exit
 popd
