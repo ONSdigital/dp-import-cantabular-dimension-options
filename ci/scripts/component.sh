bb3dd048b65cf1cbd447296f643d651f62247556
David Subiros <subiros.david@gmail.com>
2021-07-09 13:06:11 +0100

    docker/compose image for CI

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 
 pushd dp-import-cantabular-dimension-options/features/compose
-  docker compose up --abort-on-container-exit
+  echo "hello component tests"
+  docker-compose up --abort-on-container-exit
 popd
