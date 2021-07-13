d544fe50ba76d29644b9d6f6d2f30a785896f1b6
David Subiros <subiros.david@gmail.com>
2021-07-13 11:23:45 +0100

    do not preload, we potentially need to add a 'plan' block to the CI yaml file. Out of the scope of this task.

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -eux
 
 pushd dp-import-cantabular-dimension-options/features/compose
-  echo "hello component tests"
-  docker -v
-  docker-compose version
   docker-compose up --abort-on-container-exit
 popd
