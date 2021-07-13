f0c321a6a4a27217137f31a7c8272adf56d6ac37
David Subiros <subiros.david@gmail.com>
2021-07-13 16:22:50 +0100

    Added message to ignore ret err code 0

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -2,4 +2,5 @@
 
 pushd dp-import-cantabular-dimension-options/features/compose
   docker-compose up --abort-on-container-exit
+  echo "please ignore error codes 0, like so: ERRO[xxxx] 0, as error code 0 means that there was no error"
 popd
